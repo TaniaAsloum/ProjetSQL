e62aeca46e2bffef57108d9c747038c59f187efb
Unknown <tania.asloum@edu.itescia.fr>
2018-10-24 17:06:05 +0200

    fixed

--- service.sql
+++ service.sql
@@ -18,9 +18,13 @@ CREATE TABLE service(
 )
 ENGINE=INNODB;
 
+/*ALTER TABLE service
+ADD FOREIGN KEY (id_utilisateur) REFERENCES utilisateur (id) ON DELETE CASCADE ON UPDATE CASCADE*/
+
 
 INSERT INTO service (id_utilisateur, nom, description, adresse, code_postal, ville, pays, dateheure, complement)
-VALUES (2, 'service1', 'description1', 'adresse1', 'code1', 'ville1', 'pays1', '2018-10-25 00:00:00', NULL),
+VALUES 
+    (2, 'service1', 'description1', 'adresse1', 'code1', 'ville1', 'pays1', '2018-10-25 00:00:00', NULL),
     (5, 'service2', 'description2', 'adresse2', 'code2', 'ville2', 'pays2', '2018-10-25 00:00:00', NULL),
     (4, 'service3', 'description3', 'adresse3', 'code3', 'ville3', 'pays3', '2018-12-25 00:00:00', NULL),
     (6, 'service4', 'description4', 'adresse4', 'code4', 'ville4', 'pays4', '2018-10-26 00:00:00', NULL),
@@ -46,13 +50,58 @@ VALUES (2, 'service1', 'description1', 'adresse1', 'code1', 'ville1', 'pays1', '
 
 INSERT INTO service (id_utilisateur, nom, description, adresse, code_postal, ville, pays, dateheure, complement)
 VALUES (26, 'service21', 'description21', 'adresse21', 'cod21', 'ville21', 'pays21', '2018-12-25 00:00:00', NULL);
-/*
-ALTER TABLE
-ADD FOREIGN KEY (id_utilisateur) REFERENCES utilisateur (id) ON DELETE CASCADE ON UPDATE CASCADE*/
 
 
 
+/*Story 10*/
 
 
+-- SELECT *
+-- FROM service as S
+-- INNER JOIN service_utilisateur as SU
+-- ON S.id_utilisateur =! SU.id_user
+-- WHERE S.dateheure > '2018-10-24 00:00:00';
+-- ORDER BY S.dateheure ASC,
+--         S.ville ASC;
+
+SELECT S.*
+FROM service as S
+WHERE S.id NOT IN (SELECT id_utilisateur FROM service_utilisateur)
+AND S.dateheure > '2018-10-23 00:00:00'
+ORDER BY S.dateheure ASC,
+    S.ville ASC;
+
+    /*Story 11*/
+
+-- SELECT S.* , U.pseudo , U.tel_portable
+-- FROM service as S
+-- INNER JOIN utilisateur as U
+-- ON S.id_utilisateur = U.id
+
+
+-- SELECT DISTINCT S.* , U.pseudo as pseudoCreateur, U.tel_portable, U2.pseudo as pseudoInscrit
+-- FROM service as S
+-- INNER JOIN utilisateur as U
+-- ON S.id_utilisateur = U.id
+-- LEFT JOIN service_utilisateur as SU
+-- ON SU.id_service = S.id
+-- LEFT JOIN utilisateur as U2
+-- ON U2.id = SU.id_user
+-- WHERE S.id = SU.id_user;
+
+SELECT DISTINCT S.* , U.pseudo as pseudoCreateur, U.tel_portable, U2.pseudo as pseudoInscrit
+FROM service as S
+INNER JOIN utilisateur as U
+ON S.id_utilisateur = U.id
+LEFT JOIN service_utilisateur as SU
+ON SU.id_service = S.id
+LEFT JOIN utilisateur as U2
+ON U2.id = SU.id_user
+WHERE S.id = 19
+
+-- story 12 --
+
+DELETE FROM service
+WHERE id=4
 
 
